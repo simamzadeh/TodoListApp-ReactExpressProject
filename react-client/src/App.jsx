9874c03270456d7655d89d93f22265dc1c3283b0
Sima M Zadeh <77807196+simamzadeh@users.noreply.github.com>
2023-10-18 14:05:45 +0100

    Cleaning up code
    Removing unnecessary imports and simplifying code

---
+++ react-client/src/App.jsx
@@ -0,0 +1,66 @@
+import React, {Component, useEffect, useState} from "react";
+import './App.css';
+import Editor from "./LexicalEditor";
+import "./styles.css";
+import userEvent from "@testing-library/user-event";
+
+
+const App = () => {
+    const [apiResponse, setApiResponse] = useState("");
+
+    const callAPI = () => {
+        fetch("http://localhost:9000/testAPI")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }))
+            .catch(err => console.error(err));
+    }
+
+    useEffect(() => {
+        callAPI()
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Lexical Text Editor</h1>
+            <p>A place to write your to-do's!</p>
+            <Editor/>
+            {/*<input onChange={(e)=>console.log(e.target.value)}/>*/}
+            <p className="App-intro">{apiResponse}</p>
+        </div>
+    );
+}
+
+export default App;
+
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { apiResponse: "" };
+//   }
+//
+//   callAPI() {
+//     fetch("http://localhost:9000/testAPI")
+//         .then(res => res.text())
+//         .then(res => this.setState({ apiResponse: res }))
+//         .catch(err => err);
+//   }
+//
+//   componentDidMount() {
+//     this.callAPI();
+//   }
+//
+//   render() {
+//     return (
+//         <div className="App">
+//             <h1>Lexical Text Editor</h1>
+//             <p>A place to write your to-do's!</p>
+//             <Editor/>
+//             {/*<input onChange={(e)=>console.log(e.target.value)}/>*/}
+//           <p className="App-intro">{this.state.apiResponse}</p>
+//         </div>
+//     );
+//   }
+// }
+
+// export default App;
--- react-client/src/LexicalEditor.js
+++ react-client/src/LexicalEditor.js
@@ -1,13 +1,9 @@
-import {$getRoot, $getSelection} from 'lexical';
-import {useEffect} from 'react';
-
+import React from "react";
 import {LexicalComposer} from '@lexical/react/LexicalComposer';
-import {PlainTextPlugin} from '@lexical/react/LexicalPlainTextPlugin';
 import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
 import {ContentEditable} from '@lexical/react/LexicalContentEditable';
 import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
 import {OnChangePlugin} from '@lexical/react/LexicalOnChangePlugin';
-import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
 import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
 import editorConfig from "./editorConfig";
 import onChange from "./onChange";
@@ -21,8 +17,8 @@ export default function Editor() {
                     placeholder={<Placeholder/>}
                     ErrorBoundary={LexicalErrorBoundary}
                 />
-                {/*<HistoryPlugin />*/}
-                {/*<OnChangePlugin onChange={onChange} />*/}
+                <HistoryPlugin />
+                <OnChangePlugin onChange={onChange} />
             </div>
         </LexicalComposer>
     );
