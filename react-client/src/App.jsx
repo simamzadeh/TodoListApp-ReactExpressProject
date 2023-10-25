8981144aeeee6ce7ae45c6b5cfe54aaee3cae61b
Sima M Zadeh <77807196+simamzadeh@users.noreply.github.com>
2023-10-25 12:00:17 +0100

    Removing comments React App.js

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -31,36 +31,3 @@ const App = () => {
 }
 
 export default App;
-
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { apiResponse: "" };
-//   }
-//
-//   callAPI() {
-//     fetch("http://localhost:9000/testAPI")
-//         .then(res => res.text())
-//         .then(res => this.setState({ apiResponse: res }))
-//         .catch(err => err);
-//   }
-//
-//   componentDidMount() {
-//     this.callAPI();
-//   }
-//
-//   render() {
-//     return (
-//         <div className="App">
-//             <h1>Lexical Text Editor</h1>
-//             <p>A place to write your to-do's!</p>
-//             <Editor/>
-//             {/*<input onChange={(e)=>console.log(e.target.value)}/>*/}
-//           <p className="App-intro">{this.state.apiResponse}</p>
-//         </div>
-//     );
-//   }
-// }
-
-// export default App;
